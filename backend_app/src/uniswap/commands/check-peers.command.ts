ea557c1927e20d97cd3c0472f37f21c9ac50babd
Andrii Saiak <andresayak@gmail.com>
2023-04-26 22:21:58 +0200

    scan peers

--- backend_app/src/uniswap/commands/check-peers.command.ts
+++ backend_app/src/uniswap/commands/check-peers.command.ts
@@ -6,6 +6,7 @@ import {EthProviderFactoryType} from "../uniswap.providers";
 import axios from "axios";
 import * as geoip from 'geoip-lite';
 import {PeerEntity} from "../entities/peer.entity";
+import {exec} from "child_process";
 
 @Injectable()
 export class CheckPeersCommand {
@@ -38,43 +39,44 @@ export class CheckPeersCommand {
         });
         console.log('json', json);
 
-        //const filebody = fs.readFileSync("/var/www/backend_app/storage/peers.txt", 'utf-8');
-        //const json = JSON.parse(filebody);
-        const peers = json['result'];//.filter((item)=>item.network.static);
+        const peers = json['result'];
         let i = 0;
-        for(const peer of peers){
-            const [ip_address, port] = peer.network.remoteAddress.split(':');
-            console.log(++i+'/'+peers.length, ip_address+'\t', );
-            const geo = await geoip.lookup(ip_address);
-            console.log('geo', geo);
 
-            const exec = require('child_process').exec;
-            const ping = await new Promise<number | null>(done=>exec("ping -c 3 "+ip_address, function (err, stdout, stderr) {
-                if(stderr || err)
-                console.log('stderr', stderr, err);
-                const result = stdout.match(/min\/avg\/max\/mdev = [\d\.]+\/([\d\.]+)\/[\d\.]+\//);
-                done(result && result[1]?parseFloat(result[1]):null);
+        const chunkSize = 20;
+        for (let i = 0; i < peers.length; i += chunkSize) {
+            const chunk = peers.slice(i, i + chunkSize);
+            await Promise.all(chunk.map((peer, index) => {
+                return new Promise(async (done) => {
+                    const [ip_address, port] = peer.network.remoteAddress.split(':');
+                    const geo = await geoip.lookup(ip_address);
+                    const ping = await new Promise<number | null>(done => exec("ping -c 3 " + ip_address, function (err, stdout, stderr) {
+                        if (stderr || err)
+                            console.log('stderr', stderr, err);
+                        const result = stdout.match(/min\/avg\/max\/mdev = [\d\.]+\/([\d\.]+)\/[\d\.]+\//);
+                        done(result && result[1] ? parseFloat(result[1]) : null);
+                    }));
+                    console.log(++i + '/' + peers.length, ip_address + '\t', ping, geo);
+                    let peerEntity = await this.peerRepository.findOne({
+                        where: {
+                            ip_address
+                        }
+                    });
+                    if (!peerEntity) {
+                        peerEntity = new PeerEntity({});
+                    }
+                    peerEntity.fill({
+                        ip_address,
+                        port,
+                        country: geo?.country,
+                        region: geo?.region,
+                        city: geo?.city,
+                        latitude: geo?.ll[0],
+                        longitude: geo?.ll[1],
+                        ping
+                    });
+                    await this.peerRepository.save(peerEntity);
+                });
             }));
-            console.log('timePing', ping);
-            let peerEntity = await this.peerRepository.findOne({
-                where: {
-                    ip_address
-                }
-            });
-            if(!peerEntity){
-                peerEntity = new PeerEntity({});
-            }
-            peerEntity.fill({
-                ip_address,
-                port,
-                country: geo?.country,
-                region: geo?.region,
-                city: geo?.city,
-                latitude: geo?.ll[0],
-                longitude: geo?.ll[1],
-                ping
-            });
-            await this.peerRepository.save(peerEntity);
         }
         console.log('Done!');
     }
