421a5f5a7ea92a67de1c5a56baba77ca4ce99c63
Andrii Saiak <andresayak@gmail.com>
2023-05-04 17:01:12 +0300

    scan peers

---
+++ backend_app/src/uniswap/commands/check-peers-validators.command.ts
@@ -0,0 +1,66 @@
+import {Command} from 'nestjs-command';
+import {Inject, Injectable} from '@nestjs/common';
+import {Repository} from "typeorm";
+import {EnvService} from "../../env/env.service";
+import {EthProviderFactoryType} from "../uniswap.providers";
+import {PeerEntity} from "../entities/peer.entity";
+import {ValidatorEntity} from "../entities/validator.entity";
+
+@Injectable()
+export class CheckPeersValidatorsCommand {
+    constructor(private readonly envService: EnvService,
+                @Inject('PEER_REPOSITORY')
+                private readonly peerRepository: Repository<PeerEntity>,
+                @Inject('VALIDATOR_REPOSITORY')
+                private readonly validatorRepository: Repository<ValidatorEntity>,
+                @Inject('ETH_PROVIDERS')
+                private readonly providers: EthProviderFactoryType
+    ) {
+    }
+
+    @Command({
+        command: 'check:peers-validators',
+        autoExit: false
+    })
+    async create() {
+        const peers = await this.peerRepository.find();
+        const validators = await this.validatorRepository.find();
+
+        const stats = {};
+
+        for(const peer of peers){
+            if(!stats[peer.ip_address]){
+                stats[peer.ip_address] = {
+                    count: 0,
+                    validators: 0,
+                    probability: 0
+                };
+            }
+            stats[peer.ip_address]['count']++;
+            for(const validator of validators){
+                const match = validator.extra.match(/go([\d\.]+)/);
+                if(match && peer.name){
+                    const version = match[1];
+                    const reg = 'go'+version.replaceAll('.','\\.');
+                    const result = peer.name.match(new RegExp(reg,  'ig'));
+                    if(result){
+                        stats[peer.ip_address]['validators']++;
+                    }
+                }
+            }
+        }
+        console.log('items', Object.values(stats).length);
+        const items = Object.fromEntries(Object.entries(stats).filter(([index,item]:any)=>{
+            return item.validators>0;
+        }));
+        let list:any = [];
+        for(const peer of peers){
+            if(items[peer.ip_address]){
+                //list.push();
+            }
+//console.log('peer', peer);
+        }
+        console.log('items', Object.values(items).length);
+        console.log('Done!');
+    }
+}
