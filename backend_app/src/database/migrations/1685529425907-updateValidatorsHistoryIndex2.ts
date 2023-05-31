766c9635006edaa050bbe5ad64ec465ecda1572c
Andrii Saiak <andresayak@gmail.com>
2023-05-31 13:56:58 +0300

    remove index

--- backend_app/src/database/migrations/1685529425907-updateValidatorsHistoryIndex2.ts
+++ backend_app/src/database/migrations/1685529425907-updateValidatorsHistoryIndex2.ts
@@ -1,11 +1,24 @@
-import {MigrationInterface, QueryRunner, TableIndex} from "typeorm";
+import {MigrationInterface, QueryRunner, TableForeignKey, TableIndex} from "typeorm";
 
 export class updateValidatorsHistoryIndex21685529425907 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         const table = await queryRunner.getTable('validators_history');
+        const foreignKey = table.foreignKeys.find((index:TableForeignKey)=> JSON.stringify(index.columnNames)==JSON.stringify(['validator_id']));
+        if(foreignKey)
+            await queryRunner.dropForeignKey('validators_history', foreignKey.name);
         const index = table.indices.find((index:TableIndex)=> index.isUnique  && JSON.stringify(index.columnNames)==JSON.stringify(['validator_id', 'extra']));
-        await queryRunner.query("ALTER TABLE `validators_history` DROP INDEX `"+index.name+"`;")
+        if(index)
+            await queryRunner.dropIndex('validators_history', index.name);
+        await queryRunner.createForeignKey(
+            'validators_history',
+            new TableForeignKey({
+                columnNames: ['validator_id'],
+                referencedColumnNames: ['id'],
+                referencedTableName: 'validators',
+                onDelete: 'CASCADE',
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
