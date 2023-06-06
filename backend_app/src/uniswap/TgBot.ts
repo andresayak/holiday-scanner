e751acd3783d178af794c3052bc750d58aca474b
Andrii Saiak <andresayak@gmail.com>
2023-06-06 14:45:18 +0300

    MultiSwapV3

--- backend_app/src/uniswap/TgBot.ts
+++ backend_app/src/uniswap/TgBot.ts
@@ -2,6 +2,11 @@ import * as Telegram from 'node-telegram-bot-api';
 import {EnvService} from "../env/env.service";
 import {Injectable} from "@nestjs/common";
 
+const timeStamp = (timestamp: number = null) => {
+    const time = timestamp ? timestamp.toString() : new Date().getTime().toString();
+    return time.substring(0, time.length - 3) + '.' + time.substring(time.length - 3);
+}
+
 @Injectable()
 export class TgBot {
     private readonly tg?: Telegram;
@@ -15,7 +20,7 @@ export class TgBot {
         console.log('[TG]: ' + message);
         if (this.chatId && this.tg) {
             try {
-                await this.tg.sendMessage(this.chatId, this.envService.get('APP_NAME')+'\n'+message);
+                await this.tg.sendMessage(this.chatId, '[' + timeStamp() + ']'+this.envService.get('APP_NAME')+'\n'+message);
             } catch (e) {
                 console.log(e);
             }
